fa511a28698d085a975de37f913042a81650a46b
Oscar Ablinger <dev.ablinger@gmail.com>
2019-11-29 14:02:57 +0100

    feat(commonmark,tests): Create bash script to auto-create test files for Commonmark

---
+++ make-commonmark-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo Please provide the version of the commonmark spec
+  echo See https://spec.commonmark.org/
+  exit
+else
+  SPEC_VERSION=$1
+fi
+
+
+curl "https://spec.commonmark.org/$SPEC_VERSION/spec.json" |
+  jq '[.[] | select(.section == "ATX headings" or .section == "Setext headings") | {markdown, section, valid: .html | startswith("<h")}]' > tests/commonmark-$SPEC_VERSION.json
